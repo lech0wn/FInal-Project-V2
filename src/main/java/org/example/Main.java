fda680e1898e927729893b0a259b144192de308f
lech0wn <annissabal@gmail.com>
2024-11-26 11:04:15 +0800

    :)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,14 @@ import javax.swing.*;
 public class Main {
     public static  void main (String[] args) {
 
+        //TODO completed orders cannot be deleted
+        //TODO JCOMBOBOX MEALS
+        //TODO menupage out of stock meals should be at the bottom
+        //TODO use method for date, not textfield
+        //TODO order should not have price
+        //TODO searchbar every letter, press enter to search
+
+
         JFrame frame = new JFrame();
         new LoginPage(frame);
 
